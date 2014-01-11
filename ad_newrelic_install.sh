201ce73612d00b68fb3d8ab2c1781e11bb3344d0
Gilles Doge <gilles.doge@gmail.com>
2014-01-11 21:58:59 +0100

    download from archive

--- ad_newrelic_install.sh
+++ ad_newrelic_install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
- 
-NRVERSION="4.3.5.33-linux"
+
+NRVERSION="4.4.5.35"
+NRVERSIONFULL="4.4.5.35-linux"
 NRBASEDIR="$HOME/newrelic"
 MODULEDIR="$HOME/lib/php/extensions"
 # get this value from 'uname -m'
@@ -15,16 +16,16 @@ mkdir -p $MODULEDIR
 cd $NRBASEDIR
  
 echo ">> Download the New Relic Agent..."
-wget "https://download.newrelic.com/php_agent/release/newrelic-php5-$NRVERSION.tar.gz"
+wget "https://download.newrelic.com/php_agent/archive/$NRVERSION/newrelic-php5-$NRVERSIONFULL.tar.gz"
  
 echo ">> Unzip archive..."
-gzip -dc "newrelic-php5-$NRVERSION.tar.gz" | tar xf -
+gzip -dc "newrelic-php5-$NRVERSIONFULL.tar.gz" | tar xf -
  
 echo ">> Installing..."
 rm -f $MODULEDIR/newrelic.so
-ln -s $NRBASEDIR/newrelic-php5-$NRVERSION/agent/$ARCH/newrelic-${PHPAPI}${PHPZTS}.so $MODULEDIR/newrelic.so
+ln -s $NRBASEDIR/newrelic-php5-$NRVERSIONFULL/agent/$ARCH/newrelic-${PHPAPI}${PHPZTS}.so $MODULEDIR/newrelic.so
  
-rm "newrelic-php5-$NRVERSION.tar.gz"
+rm "newrelic-php5-$NRVERSIONFULL.tar.gz"
  
 echo ""
 echo ""
@@ -34,6 +35,6 @@ echo "extension=$MODULEDIR/newrelic.so"
 echo "newrelic.appname=\"$USER-global\""
 echo "newrelic.license=\"YOUR-LICENSE\""
 echo "newrelic.daemon.logfile=\"$HOME/log/newrelic-daemon.log\""
-echo "newrelic.daemon.location=\"$NRBASEDIR/newrelic-php5-$NRVERSION/daemon/newrelic-daemon.$ARCH\""
+echo "newrelic.daemon.location=\"$NRBASEDIR/newrelic-php5-$NRVERSIONFULL/daemon/newrelic-daemon.$ARCH\""
 echo "newrelic.capture_params=On"
 echo ""
