66c94db372176830e8b372a5abe525d02dee123c
Toni Fisler <mail@tonifisler.ch>
2015-03-25 14:18:04 +0100

    add npm update command and remove less installation

--- README.md
+++ README.md
@@ -1,13 +1,12 @@
 alwaysdata-utils
 ================
 
-Scripts to install nodejs, lessc, newrelic, ..., on alwaysdata servers
-
+Scripts to install nodejs, newrelic, ..., on alwaysdata servers
 
 ## Install NewRelic
 
 curl https://raw.github.com/antistatique/alwaysdata-utils/master/ad_newrelic_install.sh | bash
 
-## Install Nodejs + lesscss
+## Install Nodejs and NPM
 
 curl https://raw.githubusercontent.com/antistatique/alwaysdata-utils/master/ad_node_install.sh | bash
--- ad_node_install.sh
+++ ad_node_install.sh
@@ -2,25 +2,24 @@
 TMP_DIR="/tmp/${USER}_node"
 PREFIX_DIR="$HOME/local"
 NODE_VERSION=v0.10.20
- 
+
 mkdir -p $PREFIX_DIR
 mkdir -p $TMP_DIR
- 
+
 # INSTALL NODE.JS
 cd $TMP_DIR && git clone https://github.com/joyent/node.git .
 git checkout $NODE_VERSION
 ./configure --prefix=$PREFIX_DIR
 make
 make install
- 
+
 # INSTALL/UPGRADE NPM
 $PREFIX_DIR/bin/npm install -g npm
- 
-# INSTALL LESSC
-$PREFIX_DIR/bin/npm install -g less
- 
+$PREFIX_DIR/bin/npm update -g npm
+
 echo "export PATH=\$PATH:$PREFIX_DIR/bin" >> $HOME/.bashrc
-source ~/.bashrc 
+source ~/.bashrc
+
 # cleanup
 cd $PREFIX_DIR
 rm -r $TMP_DIR
