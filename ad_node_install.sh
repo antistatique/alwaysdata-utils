3ea2939eb3411e887c2e06a948b05615506873b3
Yann Gouffon <github@yago-art.ch>
2015-09-23 11:47:13 +0200

    change the way to install the latest version of Nodejs

--- ad_node_install.sh
+++ ad_node_install.sh
@@ -1,23 +1,14 @@
 #!/bin/sh
-TMP_DIR="/tmp/${USER}_node"
-PREFIX_DIR="$HOME/local"
-NODE_VERSION=v0.10.20
+INSTALL_DIR="$HOME"
+NODE_VERSION=v4.1.1
+NODE_DIRNAME=node-$NODE_VERSION-linux-x64
 
-mkdir -p $PREFIX_DIR
-mkdir -p $TMP_DIR
+cd $INSTALL_DIR
+wget --no-check-certificates https://nodejs.org/dist/$NODE_VERSION/$NODE_DIRNAME.tar.gz
+tar -xzf $NODE_DIRNAME.tar.gz
+rm $NODE_DIRNAME.tar.gz
 
-# INSTALL NODE.JS
-cd $TMP_DIR && git clone https://github.com/joyent/node.git .
-git checkout $NODE_VERSION
-./configure --prefix=$PREFIX_DIR
-make
-make install
-
-# INSTALL/UPGRADE NPM
-$PREFIX_DIR/bin/npm install -g npm
-$PREFIX_DIR/bin/npm update -g npm
-
-echo "export PATH=\$PATH:$PREFIX_DIR/bin" >> $HOME/.bashrc
+echo "export PATH=\$PATH:$INSTALL_DIR/$NODE_DIRNAME/bin" >> $HOME/.bashrc
 
 # Fix profile for capistrano access to npm
 echo "if [ "\$BASH" ]; then
@@ -26,9 +17,3 @@ echo "if [ "\$BASH" ]; then
   fi
 fi
 " >> $HOME/.profile
-
-source ~/.bashrc
-
-# cleanup
-cd $PREFIX_DIR
-rm -r $TMP_DIR
